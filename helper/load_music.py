8b2e8ae94a48098204e36368fb8cc7ec8b5b714f
barnrang <barnrang@gmail.com>
2017-11-26 23:47:50 +0900

    add helper

---
+++ helper/load_music.py
@@ -0,0 +1,20 @@
+from pydub import AudioSegment
+import numpy as np
+import os
+import pygame
+
+def music_load(dir='../Music_sample',load_list=[],length=5000):
+    if load_list == []:
+        load_list = os.listdir(dir)
+    all_file = os.listdir(dir)
+    out_array = []
+    load_list = list(filter(lambda x: x[-4:] == '.mp3',load_list))
+    unload = 0
+    for el in load_list:
+        if el not in all_file:
+            unload = 1
+            print('Warning, file {} not found'.format(el))
+            continue
+        file_dir = os.path.join(dir,el)
+        out_array.append(AudioSegment.from_mp3(file_dir).get_array_of_samples()[:length].tolist())
+    return np.array(out_array,dtype='int16')
